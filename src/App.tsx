001fc9843edc2365f0758af01e54a14b59f19050
Barun Pandey <apple@Apples-MacBook-Pro.local>
2021-02-07 09:09:48 +0545

    initialise repo with typescript eslint and prettier configured

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, Text, StatusBar } from 'react-native';
+
+const App = () => {
+	return (
+		<>
+			<StatusBar barStyle="dark-content" />
+			<SafeAreaView>
+				<Text>Hello</Text>
+			</SafeAreaView>
+		</>
+	);
+};
+
+export default App;
