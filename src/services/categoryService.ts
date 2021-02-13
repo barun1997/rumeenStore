7a68f0f60a769aae33717a41a4a3770dd5980b48
Barun Pandey <apple@Apples-MacBook-Pro.local>
2021-02-13 09:16:12 +0545

    configure product form so it adds product to firebase

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,25 +3,22 @@ import { CategoryType } from '../interfaces/Category';
 
 const categoryCollection = firestore().collection('stores').doc('barun').collection('categories');
 
-const addCategory = async (category: CategoryType): Promise<boolean | undefined> => {
-	try {
-		await categoryCollection.doc(category.name).set(category);
-		return true;
-	} catch (error) {
-		console.log(error);
-	}
+const addCategory = async (category: CategoryType): Promise<boolean> => {
+	const categoryDoc = categoryCollection.doc(category.name);
+	const categoryPresent = await categoryDoc.get();
+
+	if (categoryPresent) throw Error('The category is already present');
+
+	await categoryCollection.doc(category.name).set(category);
+	return true;
 };
 
-const getCategories = async (): Promise<CategoryType[] | undefined> => {
-	try {
-		const result = await categoryCollection.get();
+const getCategories = async (): Promise<CategoryType[]> => {
+	const result = await categoryCollection.get();
 
-		const categories = result.docs.map((doc) => doc.data() as CategoryType);
+	const categories = result.docs.map((doc) => doc.data() as CategoryType);
 
-		return categories;
-	} catch (error) {
-		console.log(error);
-	}
+	return categories;
 };
 
 export { addCategory, getCategories };
