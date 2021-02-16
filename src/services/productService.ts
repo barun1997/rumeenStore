637251f64794de4c1322e6cadf639e2824c5d791
Barun Pandey <apple@Apples-MacBook-Pro.local>
2021-02-16 08:56:42 +0545

    add validation for category form

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,10 +1,19 @@
 import firestore from '@react-native-firebase/firestore';
 import { ProductType } from '../interfaces/Product';
+import { fieldIncrementByOne } from '../constants/fieldIncrementByOne';
 
 const productCollection = firestore().collection('stores').doc('barun').collection('products');
+const storeDocument = firestore().collection('stores').doc('barun');
+const categoryCollection = firestore().collection('stores').doc('barun').collection('categories');
 
 const addProduct = async (product: ProductType): Promise<boolean> => {
 	await productCollection.add(product);
+	await storeDocument.update({
+		productCount: fieldIncrementByOne,
+	});
+	await categoryCollection.doc(product.category).update({
+		count: fieldIncrementByOne,
+	});
 	return true;
 };
 
