1cefe1febf703a29d5e0ab00a99ba74357197969
Barun Pandey <apple@Apples-MacBook-Pro.local>
2021-02-23 09:28:19 +0545

    configure initializing screen as a intermediate between guest and main

--- src/app.options.ts
+++ src/app.options.ts
@@ -1,5 +1,10 @@
 import { StackNavigationOptions } from '@react-navigation/stack';
 
+export const useGuestOptions = (): StackNavigationOptions => ({
+	title: 'Dailo - Service for your needs',
+	headerShown: false,
+});
+
 export const useMainOptions = (): StackNavigationOptions => ({
 	headerShown: false,
 });
